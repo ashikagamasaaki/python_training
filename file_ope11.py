83b0c9482fa11ca5c6c0fb55fe3812f4abdf9c4c
ashikagamasaaki <gamasaga0312@gmail.com>
2024-01-31 09:28:23 +0900

    ファイル操作演習

--- file_ope11.py
+++ file_ope11.py
@@ -34,3 +34,67 @@
 
 このデータを読み取り、各生徒の科目ごとの平均点を計算して、新しいJSONファイル average_grades.json に出力してください。
 """
+
+import json
+
+with open('resources/grades.json', 'r', encoding='utf-8') as f, open('resources/new_agerage_grades.json', 'w', encoding='utf-8') as nf:
+  json_data = json.load(f)
+  school_class = json_data.get('class')
+  grades_list = json_data.get('students')
+  subjects = grades_list[0].get('grades').keys()
+
+  new_json = {"class": school_class, "averages": {}}
+  averages = new_json.get('averages')
+  
+  for s in subjects:
+    averages[s] = {"total": 0, "count": 0, "average": 0}
+
+  for l in grades_list:
+    grades_data = l.get('grades')    
+    for sub, val in grades_data.items():
+      averages[sub]["total"] = averages[sub]["total"] + val
+      averages[sub]["count"] = averages[sub]["count"] + 1
+    
+    
+  for key in averages.keys():
+    averages[key]["average"] = averages[key]["total"] / averages[key]["count"]
+
+
+
+  json.dump(new_json, nf, indent=2)
+
+
+
+
+# with open('resources/grades.json', 'r', encoding='utf-8') as f, open('resources/agerage_grades.json', 'w', encoding='utf-8') as nf:
+#   data = json.load(f)
+  
+#   class_name = data.get('class', '')
+#   students = data.get('students', [])
+
+#   averages = {'class': class_name, 'averages': {}}
+
+#   for student in students:
+#       student_name = student.get('name', '')
+#       grades = student.get('grades', {})
+      
+#       for subject, score in grades.items():
+#           if subject not in averages['averages']:
+#               averages['averages'][subject] = {'total': 0, 'count': 0}
+          
+#           averages['averages'][subject]['total'] += score
+#           averages['averages'][subject]['count'] += 1
+
+#   for subject, info in averages['averages'].items():
+#       averages['averages'][subject]['average'] = info['total'] / info['count']
+
+#   json.dump(averages, nf, indent=2)
+  
+  
+  
+  
+  
+  
+  
+  
+  
--- gpt05.py
+++ gpt05.py
@@ -12,14 +12,60 @@ Beautiful Soup: 与えられたHTMLページから特定の要素をBeautiful So
 Scikit-learn: 与えられたデータセットを使用して、Scikit-learnを使って単純な機械学習モデルを訓練してください。
 TensorFlow/Keras: 与えられたデータを使用して、TensorFlowとKerasを使ってニューラルネットワークを構築してください。
 """
+import pandas as pd
 
-# Pandas: 与えられた辞書 data を使用して、PandasのDataFrameを作成してください。
-# Pandas: 与えられたPandasのDataFrame df から特定の列を選択して、新しいDataFrameを作成してください。
-# Pandas: 与えられたPandasのDataFrame df から欠損値（NaN）を含む行を削除してください。
-# Matplotlib: 与えられたリスト x と y を使用して、Matplotlibを使って散布図を描画してください。
-# Matplotlib: 与えられたNumPy配列 data を使用して、Matplotlibを使ってヒストグラムを描画してください。
-# Seaborn: 与えられたPandasのDataFrame df を使用して、Seabornを使ってボックスプロットを描画してください。
-# Requests: 与えられたURLからデータを取得して、Requestsライブラリを使ってHTTPリクエストを行ってください。
-# Beautiful Soup: 与えられたHTMLページから特定の要素をBeautiful Soupを使って抽出してください。
-# Scikit-learn: 与えられたデータセットを使用して、Scikit-learnを使って単純な機械学習モデルを訓練してください。
-# TensorFlow/Keras: 与えられたデータを使用して、TensorFlowとKerasを使ってニューラルネットワークを構築してください。
+# 41    Pandas: 与えられた辞書 data を使用して、PandasのDataFrameを作成してください。
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'San Francisco', 'Los Angeles']}
+df = pd.DataFrame(data)
+print(df)
+
+
+# 42    Pandas: 与えられたPandasのDataFrame df から特定の列を選択して、新しいDataFrameを作成してください。
+name_df = df[['Name']]
+print(name_df)
+
+# 43    Pandas: 与えられたPandasのDataFrame df から欠損値（NaN）を含む行を削除してください。
+nan_data = {'Name': ['Alice', None, 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'San Francisco', 'Los Angeles']}
+df = pd.DataFrame(nan_data)
+df_no_missing = df.dropna()
+print(df_no_missing)
+
+
+# 44    Matplotlib: 与えられたリスト x と y を使用して、Matplotlibを使って散布図を描画してください。
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 12, 8, 15, 20]
+
+# plt.plot(x, y)
+# plt.scatter(x, y)
+# plt.show()
+
+
+# 45    Matplotlib: 与えられたNumPy配列 data を使用して、Matplotlibを使ってヒストグラムを描画してください。
+import numpy as np
+data = np.random.randn(1000)
+# plt.hist(data, bins=20, color='skyblue', edgecolor='black')
+# plt.show()
+
+
+# 46    Seaborn: 与えられたPandasのDataFrame df を使用して、Seabornを使ってボックスプロットを描画してください。
+import seaborn as sns
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE'],
+        'Age': [25, 30, 35, 11, 89, 23, 55, 12],
+        'City': ['New York', 'San Francisco', 'Los Angeles', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE']}
+df = pd.DataFrame(data)
+sns.boxplot(x="Age", data=df)
+plt.show()
+
+
+# 47    Requests: 与えられたURLからデータを取得して、Requestsライブラリを使ってHTTPリクエストを行ってください。
+
+
+# 48    Beautiful Soup: 与えられたHTMLページから特定の要素をBeautiful Soupを使って抽出してください。
+# 49    Scikit-learn: 与えられたデータセットを使用して、Scikit-learnを使って単純な機械学習モデルを訓練してください。
+# 50    TensorFlow/Keras: 与えられたデータを使用して、TensorFlowとKerasを使ってニューラルネットワークを構築してください。
